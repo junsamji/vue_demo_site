f378ce8c285a44a3116bc84e26374d2c9f723569
지준삼 <jsji@daou.co.kr>
2022-10-20 10:44:44 +0900

    외부데이터 가져와서 import해보기

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,74 @@
 <template>
+  <div class="black-bg" v-if="modal">
+    <div class="white-bg">
+      <h4>상세</h4>
+      <p>상세내용</p>
+      <button @click="modal = false">닫기</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
   </div>
 
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <div v-for="(item, i) in products" :key="i">
+  <div v-for="item in products" :key="item.id">
+    <img :src="item.image" class="item-img" @click="modal = true" />
     <h4>{{ item.label }}</h4>
     <p>{{ item.price }}</p>
+    <button @click="item.joiner++">♥</button
+    ><span> 좋아요 : {{ item.joiner }}</span>
   </div>
 </template>
 
 <script>
+import _products from './assets/products'
+
 export default {
   name: 'App',
   data() {
     return {
-      menus: ['Home', 'Products', 'About'],
-      products: [
-        { label: '약정(무료)', price: 0 },
-        { label: '약정(유료)', price: 1000000 },
-        { label: '약정(무상)', price: 0 }
-      ]
+      menus: ['Home', 'Shop', 'About'],
+      modal: false,
+      products: _products
     }
   },
+  methods: {},
   components: {}
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.item-img {
+  width: 30%;
+  margin-top: 40px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 .menu {
